6699d18ef3f2a3e221f7c5283482cb553c5ddbc9
komarovae <dethloony@yandex.ru>
2022-11-17 23:47:31 +0300

    first commit

--- BX_AJAX.php
+++ BX_AJAX.php
@@ -4,9 +4,14 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 //устанавливаем заголовок страницы
 $APPLICATION->SetTitle("AJAX");
 
+
+//Подключаем ядро аякс
    CJSCore::Init(array('ajax'));
+//создаем переменную для проверки
    $sidAjax = 'testAjax';
+
 if(isset($_REQUEST['ajax_form']) && $_REQUEST['ajax_form'] == $sidAjax){
+   //вывод аякс результата. если все ок привет, в ином случае пусто.
    $GLOBALS['APPLICATION']->RestartBuffer();
    echo CUtil::PhpToJSObject(array(
             'RESULT' => 'HELLO',
@@ -16,12 +21,19 @@ if(isset($_REQUEST['ajax_form']) && $_REQUEST['ajax_form'] == $sidAjax){
 }
 
 ?>
+
+<!--верстка блоков-->
 <div class="group">
    <div id="block"></div >
    <div id="process">wait ... </div >
 </div>
+
+
 <script>
+//Если определить window.BXDEBUG = true; То можно вывести в консоль значение и стек: BX.debug('AJAX-DEMOResponse ', data);
    window.BXDEBUG = true;
+
+//демо загрузка, пока получаем дсоны видим надпись вайт...
 function DEMOLoad(){
    BX.hide(BX("block"));
    BX.show(BX("process"));
@@ -30,6 +42,8 @@ function DEMOLoad(){
       DEMOResponse
    );
 }
+
+//в блок выводим даные data.RESULT
 function DEMOResponse (data){
    BX.debug('AJAX-DEMOResponse ', data);
    BX("block").innerHTML = data.RESULT;
@@ -42,15 +56,20 @@ function DEMOResponse (data){
    );
 }
 
+
 BX.ready(function(){
    /*
    BX.addCustomEvent(BX("block"), 'DEMOUpdate', function(){
       window.location.href = window.location.href;
    });
    */
+  
+//При загрузке страницы скрывает верстку с классом блок и процесс
    BX.hide(BX("block"));
    BX.hide(BX("process"));
    
+
+   //при нажатии клик ми вызывает обработчик событий
     BX.bindDelegate(
       document.body, 'click', {className: 'css_ajax' },
       function(e){
@@ -65,6 +84,7 @@ BX.ready(function(){
 });
 
 </script>
+
 <div class="css_ajax">click Me</div>
 <?
 //подключаем эпилог ядра bitrix
